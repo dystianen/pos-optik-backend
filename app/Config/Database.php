8d03d51680cd24da4c2a6d4898c63acc1c230837
Dystian En Y <46418294+dystianen@users.noreply.github.com>
2025-06-26 20:58:21 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,9 +29,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'pos_optik',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
