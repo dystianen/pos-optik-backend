fb7e0b7931a1a0cf6913c7f9bcebbad1b86d881c
Dystian En <distian.enyus@gmail.com>
2025-07-03 16:08:51 +0700

    update favicon

--- app/Views/layouts/l_dashboard.php
+++ app/Views/layouts/l_dashboard.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets'); ?>/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="favicon.ico">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>/favicon.ico">
+  <link rel="icon" href="<?= base_url(); ?>/favicon.ico">
   <title>
     Optikers
   </title>
